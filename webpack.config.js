3c73067c115fe26587e965ee95e293faeb7fe815
tb <t.bak@selleo.com>
2016-03-07 11:08:23 +0100

    test issue 51 - 2.1.3 plugin version

--- webpack.config.js
+++ webpack.config.js
@@ -21,14 +21,18 @@ module.exports = {
   devtool:"#inline-source-map",
   postcss: [ autoprefixer({ browsers: ['last 2 versions'] }) ],
   plugins: [
-
-    new SvgStore(path.join(__dirname, 'svg-source', '**/*.svg'), path.join('sprites'), {
-      name: '[hash].sprite.svg',
-      chunk: 'app',
-      prefix: 'icon-',
-      svgoOptions: {
-        // options for svgo, optional
-      }
-    })
+    new SvgStore(
+      path.join(__dirname, 'src/icons/*.svg'),
+      path.join(__dirname, 'dist/icons'),
+      {
+        name: '[hash].icons.svg',
+        chunk: 'app',
+        prefix: '',
+        svgoOptions: {
+          plugins: [
+            {removeStyleElement: true}
+          ]
+        }
+      })
   ]
 };
